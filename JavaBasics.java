f13471508391a054ce5bc3fc372f0bfe31fae28c
Tushar Goswami <122559946+tushargoswami23@users.noreply.github.com>
2023-03-16 02:34:54 +0530

    Learning Java

---
+++ JavaBasics.java
@@ -0,0 +1,12 @@
+// code for calculator first program in java.
+import java.util.*;
+public class JavaBasics {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}

6a528ce9fbf1a9c5451858e05b62c1b1e236f7d3
Tushar Goswami <122559946+tushargoswami23@users.noreply.github.com>
2023-03-17 01:34:32 +0530

    Learning java
    Code of product of two number in java

---
+++ product.java
@@ -0,0 +1,11 @@
+// Product of two numbers
+import java.util.*;
+public class product {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int Product = a * b;
+        System.out.println(Product);
+    }
+}
